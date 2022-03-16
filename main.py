352c5b681f24fe5ab1d091b6f49b83b9d874ce6a
WildanAruman <wildan.aruman90@gmail.com>
2022-03-16 12:01:03 +0700

    analisa website

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print(f'dd')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.indeed.com/jobs?'
+params = {
+    'q' : 'Python Developer',
+    'l' : 'New York State'
+}
+
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+}
+res = requests.get(url, params=params, headers=headers)
+print(res.status_code)
+soup = BeautifulSoup(res.text, 'html.parser')
+print(soup.prettify())
